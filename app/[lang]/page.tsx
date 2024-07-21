e7ddc8776315732c1c0f27ddc53397630cb85533
Manu Morante <manu@estadologico.com>
2024-07-21 02:32:49 +0200

    add footer and clean files

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,6 +1,6 @@
 import cx from "clsx"
 import { getDictionary } from "@/util/dictionary"
-import { Logo } from "@/components"
+import { Footer, Logo } from "@/components"
 import Link from "next/link"
 
 export default async function Home({
@@ -9,14 +9,14 @@ export default async function Home({
   params: { lang: string }
 }) {
   const { d, t } = getDictionary(lang)
-  const headerCx = cx("Header", "fixed top-6 left-0 right-0 z-50")
+  const headerCx = cx("Header", "fixed top-6 left-6 md:left-0 right-0 z-50")
   const videoCx = "w-screen h-screen object-cover"
 
   return (
     <>
       <div className={headerCx}>
-        <div className="container">
-          <div className="inline-block rounded-full backdrop-filter backdrop-blur-lg bg-neutral-400/20 px-6 py-3">
+        <div className="md:container">
+          <div className="inline-block">
             <div className="flex gap-2">
               <Logo />
               <Link
@@ -51,6 +51,8 @@ export default async function Home({
           media="(max-width: 600px)"
         />
       </video>
+
+      <Footer lang={lang} />
     </>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,71 +6,75 @@ import Facebook from "./icons/Facebook"
 import Instagram from "./icons/Instagram"
 
 export default function Footer({ lang }: { lang?: any }) {
-  const mainCx = cx("Footer py-10", "lg:container")
+  const mainCx = cx("Footer", "py-12 bg-neutral-900")
 
   return (
     <div className={mainCx}>
-      {/* Colaboradores */}
-      <div className="bg-neutral-400 lg:rounded-md mb-10">
-        <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-4 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <Image
-            src="/logos/la-maquina-de-escribir-logo.png"
-            alt="La máquina de escribir"
-            className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
-            width={400}
-            height={400}
-          />
-          <Image
-            src="/logos/ayntamiento-atarfe-logo.png"
-            alt="Ayuntamiento de Atarfe"
-            className="col-span-2 max-h-36 w-full object-contain lg:col-span-1"
-            width={400}
-            height={400}
-          />
-          <Image
-            src="/logos/latido-logo.png"
-            alt="Latido"
-            className="col-span-2 max-h-28 w-full object-contain lg:col-span-1"
-            width={400}
-            height={400}
-          />
-          <Image
-            src="/logos/mordisco-logo.png"
-            alt="Mordisco"
-            className="col-span-2 max-h-36 w-full object-contain lg:col-span-1"
-            width={400}
-            height={400}
-          />
-          <Image
-            src="/logos/tickbox-logo.png"
-            alt="Tickbox"
-            className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
-            width={400}
-            height={400}
-          />
-        </div>
-      </div>
-      <div className="px-6">
-        <div className="w-full flex justify-between items-center">
-          {/* Social */}
-          <div className="flex flex-wrap gap-5 content-center [&_a]:no-underline ">
-            <Link
-              href="https://www.instagram.com/ritmo_festival/"
-              target="_blank"
-            >
-              <Instagram />
-            </Link>
-
-            <Link href="https://www.facebook.com/Ritmofestival" target="_blank">
-              <Facebook />
-            </Link>
+      <div className="container">
+        <div className="w-full flex flex-col md:flex-row gap-12 justify-between items-center">
+          {/* Colaboradores */}
+          <div className="flex flex-wrap gap-5 sm:gap-10 items-center">
+            <Image
+              src="/logos/la-maquina-de-escribir-logo.png"
+              alt="La máquina de escribir"
+              className="w-16 h-auto"
+              width={269}
+              height={150}
+            />
+            <Image
+              src="/logos/ayntamiento-atarfe-logo.png"
+              alt="Ayuntamiento de Atarfe"
+              className="w-28 h-auto"
+              width={358}
+              height={212}
+            />
+            <Image
+              src="/logos/latido-logo.png"
+              alt="Latido"
+              className="w-24 h-auto"
+              width={371}
+              height={112}
+            />
+            <Image
+              src="/logos/mordisco-logo.png"
+              alt="Mordisco"
+              className="w-28 h-auto"
+              width={363}
+              height={60}
+            />
+            <Image
+              src="/logos/tickbox-logo.png"
+              alt="Tickbox"
+              className="w-20 h-auto"
+              width={400}
+              height={400}
+            />
           </div>
 
-          {/* Languge */}
-          <LocaleSwitcher lang={lang} />
-        </div>
+          <div className="flex flex-wrap gap-3 sm:gap-6 items-center">
+            <span className="text-white text-sm">Copyright © RITMO 2024</span>
 
-        <p className="text-gray my-6">Copyright © RITMO 2024</p>
+            {/* Social */}
+            <div className="flex flex-wrap gap-5 content-center [&_a]:no-underline ">
+              <Link
+                href="https://www.instagram.com/ritmo_festival/"
+                target="_blank"
+              >
+                <Instagram />
+              </Link>
+
+              <Link
+                href="https://www.facebook.com/Ritmofestival"
+                target="_blank"
+              >
+                <Facebook />
+              </Link>
+            </div>
+
+            {/* Languge */}
+            <LocaleSwitcher lang={lang} />
+          </div>
+        </div>
       </div>
     </div>
   )
