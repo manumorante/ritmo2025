265d40d4adf3c6db59eb11f47e9663ad53a05a02
Manu Morante <manu@estadologico.com>
2024-09-25 17:00:51 +0200

    hide newsletter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,11 +12,11 @@ export default function Footer({ lang }: { lang?: any }) {
 
   return (
     <>
-      <div className="py-12 bg-neutral-100">
+      {/* <div className="py-12 bg-neutral-100">
         <div className="container">
           <div className="ml-embedded" data-form="8Sa5YY"></div>
         </div>
-      </div>
+      </div> */}
 
       <div className={mainCx}>
         <div className="container">
