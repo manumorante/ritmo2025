14bbab61304210be3a7197138ef4d23ec3111b1e
Manu Morante <manu@estadologico.com>
2024-12-29 14:35:08 +0100

    feat: enhance Airports page layout and improve Spanish localization for bus travel information

--- app/[lang]/airports/page.tsx
+++ app/[lang]/airports/page.tsx
@@ -1,6 +1,7 @@
 import cx from "clsx"
 import { getDictionary } from "@/util/dictionary"
 import { HeroImage } from "@/components"
+import Image from "next/image"
 
 export default async function AirportsPage({
   params: { lang },
@@ -9,41 +10,44 @@ export default async function AirportsPage({
 }) {
   const { t } = getDictionary(lang)
 
-  const optionsCx = cx("grid grid-cols-1 sm:grid-cols-2 gap-10")
   const optionCx = cx("[&>p]:py-3")
-  const optionTitleCx = cx("text-2xl line-clamp-3")
-  const boxCx = cx("p-4 sm:p-6 border border-neutral-500/50 rounded mb-6")
-  const boxTitleCx = cx("text-xl font-light text-gray pt-0")
+  const optionTitleCx = cx("text-primary text-5xl font-light pt-0 mt-12 mb-2")
+  const boxTitleCx = cx("text-4xl font-light pt-0 mb-2")
 
   return (
     <main className="Page Airports">
+      <Image
+        className="w-full h-[40vh] sm:h-[300px] object-cover"
+        src="/air.jpg"
+        width={1120}
+        height={721}
+        alt="Img"
+      />
+
       <div className="header-flat" />
 
       <div className="container">
-        <h2 className="pageTitle">{t("airports.title")}</h2>
-        <p
-          className="text-lg"
-          dangerouslySetInnerHTML={{ __html: t("airports.intro") }}
-        />
-      </div>
+        <div className="max-w-md mx-auto">
+          <h2 className="pageTitle">{t("airports.title")}</h2>
+          <p
+            className="text-lg"
+            dangerouslySetInnerHTML={{ __html: t("airports.intro") }}
+          />
 
-      <HeroImage src="/air.jpg" />
-
-      <div className="container">
-        <div className={optionsCx}>
           <div className={optionCx}>
             <h3 className={optionTitleCx}>{t("airports.granada.title")}</h3>
             <p>{t("airports.granada.text")}</p>
-            <p>{t("airports.granada.how")}</p>
 
-            <div className={boxCx}>
-              <p className={boxTitleCx}>{t("airports.taxi")}</p>
-              <p>{t("airports.granada.taxi")}</p>
-            </div>
+            <div className="flex gap-6">
+              <div className="flex-[1]">
+                <p className={boxTitleCx}>{t("airports.taxi")}</p>
+                <p>{t("airports.granada.taxi")}</p>
+              </div>
 
-            <div className={boxCx}>
-              <p className={boxTitleCx}>{t("airports.bus")}</p>
-              <p>{t("airports.granada.bus")}</p>
+              <div className="flex-[2]">
+                <p className={boxTitleCx}>{t("airports.bus")}</p>
+                <p>{t("airports.granada.bus")}</p>
+              </div>
             </div>
           </div>
 
@@ -51,15 +55,16 @@ export default async function AirportsPage({
             <h3 className={optionTitleCx}>{t("airports.malaga.title")}</h3>
 
             <p>{t("airports.malaga.text")}</p>
-            <p>{t("airports.malaga.how")}</p>
-            <div className={boxCx}>
-              <p className={boxTitleCx}>{t("airports.bus")}</p>
-              <p>{t("airports.malaga.bus")}</p>
-            </div>
+            <div className="flex gap-6">
+              <div className="flex-[1]">
+                <p className={boxTitleCx}>{t("airports.bus")}</p>
+                <p>{t("airports.malaga.bus")}</p>
+              </div>
 
-            <div className={boxCx}>
-              <p className={boxTitleCx}>{t("airports.rent")}</p>
-              <p>{t("airports.malaga.rent")}</p>
+              <div className="flex-[2]">
+                <p className={boxTitleCx}>{t("airports.rent")}</p>
+                <p>{t("airports.malaga.rent")}</p>
+              </div>
             </div>
           </div>
         </div>
