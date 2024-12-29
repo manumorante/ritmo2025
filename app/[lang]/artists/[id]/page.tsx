ae9aba6e79eb2802e46d6d6242c44507e8a88762
Manu Morante <manu@estadologico.com>
2024-12-29 14:13:57 +0100

    feat: add navigation for next and previous artists on artist page

--- app/[lang]/artists/[id]/page.tsx
+++ app/[lang]/artists/[id]/page.tsx
@@ -1,6 +1,12 @@
 import { getDictionary } from "@/util/dictionary"
-import { getArtistById } from "@/util/data"
+import {
+  getArtistById,
+  getNextArtistById,
+  getPreviousArtistById,
+} from "@/util/data"
 import Image from "next/image"
+import { BuyButton } from "@/components"
+import Link from "next/link"
 
 export default async function Artists({
   params: { lang, id },
@@ -9,6 +15,8 @@ export default async function Artists({
 }) {
   const { t } = getDictionary(lang)
   const artist = getArtistById(id)
+  const nextArtist = getNextArtistById(id)
+  const previousArtist = getPreviousArtistById(id)
   const playerOptions =
     "color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false"
 
@@ -33,16 +41,16 @@ export default async function Artists({
       </div>
 
       <div className="container">
-        <div className="lg:flex gap-8">
-          <div className="lg:flex-[2] mb-6">
-            {t(`artists.${id}.intro`).length > 45 && (
-              <p className="lg:flex-[2] text-2xl lg:text-xl">
-                {t(`artists.${id}.intro`)}
-              </p>
-            )}
+        <div className=" mb-6">
+          {t(`artists.${id}.intro`).length > 45 && (
+            <p className="text-2xl lg:text-4xl mb-14 lg:mb-24">
+              {t(`artists.${id}.intro`)}
+            </p>
+          )}
 
+          <div className="max-w-lg mx-auto">
             {artist?.session && (
-              <div className="my-10">
+              <div className="mb-10">
                 <iframe
                   width="100%"
                   height={166}
@@ -52,14 +60,34 @@ export default async function Artists({
                 />
               </div>
             )}
-          </div>
 
-          {t(`artists.${id}.bio`) && (
-            <div
-              className="lg:flex-[2] text-xl lg:text-xl px-4 lg:pl-6"
-              dangerouslySetInnerHTML={{ __html: t(`artists.${id}.bio`) }}
-            />
-          )}
+            {t(`artists.${id}.bio`) && (
+              <div
+                className="text-xl lg:text-xl px-4 "
+                dangerouslySetInnerHTML={{ __html: t(`artists.${id}.bio`) }}
+              />
+            )}
+
+            <div className="mt-16 flex justify-center">
+              <BuyButton lang={lang} t={t} />
+            </div>
+
+            <div className="mt-16 flex justify-between items-center">
+              <Link
+                className="text-primary text-2xl no-underline"
+                href={`/${lang}/artists/${previousArtist?.id}`}
+              >
+                ← {previousArtist?.name}
+              </Link>
+
+              <Link
+                className="text-primary text-2xl no-underline"
+                href={`/${lang}/artists/${nextArtist?.id}`}
+              >
+                {nextArtist?.name} →
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </main>
--- util/data.ts
+++ util/data.ts
@@ -24,6 +24,16 @@ export function getArtistById(id: string) {
   return lineup.find((artist) => artist.id === id)
 }
 
+export function getNextArtistById(id: string) {
+  const index = lineup.findIndex((artist) => artist.id === id)
+  return lineup[index + 1] || lineup[0]
+}
+
+export function getPreviousArtistById(id: string) {
+  const index = lineup.findIndex((artist) => artist.id === id)
+  return lineup[index - 1] || lineup[lineup.length - 1]
+}
+
 export function getMenuItems({ lang }: { lang: string }) {
   return _dictionary({ lang }).menu as Links
 }
