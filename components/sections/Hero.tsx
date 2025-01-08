0e8dff78548238703c3eb97fb8d06dc5cef65ee5
Manu Morante <manu@estadologico.com>
2025-01-08 21:09:24 +0100

    feat: add animated arrow to Hero section for enhanced visual guidance

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -17,16 +17,10 @@ export default function HeroSection({
   const { scrollYProgress } = useScroll()
   const y = useTransform(scrollYProgress, [0, 1], ["0px", "600px"])
   const opacity = useTransform(scrollYProgress, [0, 0.8], [0, 1])
+  const arrowY = useTransform(scrollYProgress, [0, 0.1], ["0%", "140%"])
   return (
-    <section
-      ref={sectionRef}
-      className="relative h-screen flex flex-col justify-center"
-    >
-      <motion.div
-        className="absolute inset-0 bg-black"
-        style={{ opacity: opacity }}
-      />
-
+    <section ref={sectionRef} className="relative h-svh flex flex-col justify-center">
+      <motion.div className="absolute inset-0 bg-black" style={{ opacity: opacity }} />
       {/* Video */}
       <motion.div className="absolute inset-0 -z-10" style={{ y }}>
         <video
@@ -50,7 +44,6 @@ export default function HeroSection({
           />
         </video>
       </motion.div>
-
       {/* Text content */}
       <motion.div
         className="HeroContent container flex flex-col items-center text-center"
@@ -59,6 +52,23 @@ export default function HeroSection({
         <p className="text-2xl text-white font-light mb-4">{text}</p>
 
         <Button href={`/${lang}/tickets`}>{button}</Button>
+      </motion.div>
+
+      <motion.div
+        className="absolute w-full bottom-0 flex justify-center text-center p-5"
+        style={{ y: arrowY }}
+      >
+        <svg
+          className="animate-bounce w-8 h-8 text-white"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+        </svg>
       </motion.div>
     </section>
   )
