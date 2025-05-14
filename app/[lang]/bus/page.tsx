630190ce0482eccc7ee20a42a443ace1e7018354
Manu Morante <manu@estadologico.com>
2025-05-14 09:27:16 +0200

    feat: add BusPage component and update language files for bus ticket information

---
+++ app/[lang]/bus/page.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/components"
+import { getDictionary } from "@/lib/i18n"
+import Image from "next/image"
+
+export default async function BusPage({ params: { lang } }: { params: { lang: string } }) {
+  const { t } = getDictionary(lang)
+
+  return (
+    <main className="Page Bus">
+      <div className="header-flat" />
+
+      <div className="container">
+        <div className="max-w-md mx-auto">
+          <Image
+            className=" object-cover w-full"
+            src="/bus.jpg"
+            alt="Bus"
+            width={688}
+            height={393}
+          />
+          <h2
+            className="text-primary text-4xl lg:text-6xl text-center mt-10 mb-6  mx-7"
+            dangerouslySetInnerHTML={{ __html: t("bus.title") }}
+          />
+
+          <p
+            className="text-xl mb-8"
+            dangerouslySetInnerHTML={{
+              __html: t("bus.intro"),
+            }}
+          />
+
+          <div className="space-y-3">
+            {Array.from({ length: 8 }, (_, i) => (
+              <p
+                key={i}
+                className="text-xl"
+                dangerouslySetInnerHTML={{ __html: t(`bus.p${i + 1}`) }}
+              />
+            ))}
+          </div>
+
+          <div className="mx-auto mt-12 flex justify-center">
+            <Button href={t("tickets.bus")} target="_blank">
+              <div>{t("generic.buyTickets")}</div>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
