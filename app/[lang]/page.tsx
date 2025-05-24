cab8505b4b01c10855b67d83bad8ed7f6aa851b6
Manu Morante <manu@estadologico.com>
2025-05-24 14:04:23 +0200

    feat: add schedule button to homepage and update language files for new schedule translations

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,5 @@
 import { getDictionary } from "@/lib/i18n"
-import { Hero, Lineup, Photos, Location, FeaturedFAQs, HomeImage } from "@/components"
+import { Hero, Lineup, Photos, Location, FeaturedFAQs, HomeImage, Button } from "@/components"
 
 export default async function Home({ params: { lang } }: { params: { lang: string } }) {
   const { d, t } = getDictionary(lang)
@@ -9,6 +9,11 @@ export default async function Home({ params: { lang } }: { params: { lang: strin
       <Hero lang={lang} button={t("generic.buyTickets")} text={t("about.text")} />
       <div className="header-flat" />
       <Lineup lang={lang} t={t} />
+      <div className="mx-auto mb-24 flex justify-center">
+        <Button href={`${lang}/lineup/friday`}>
+          <div>{t("generic.schedule")}</div>
+        </Button>
+      </div>
       <HomeImage />
       <Location t={t} />
       <Photos />
